f4c40f3a3cedaa41ea968098c6e328fcd238cff5
Marcelo Jesualdo <fjesualdo@outlook.com>
2022-04-05 15:33:33 -0300

    M03V03 - Configuracao do Identity

--- src/Services/MJ.Solutions.Identidade.API/Startup.cs
+++ src/Services/MJ.Solutions.Identidade.API/Startup.cs
@@ -1,12 +1,15 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using MJ.Solutions.Identidade.API.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,9 +26,15 @@ namespace MJ.Solutions.Identidade.API
 
 		public IConfiguration Configuration { get; }
 
-		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddDbContext<ApplicationDbContext>(
+				optionsAction: options => options.UseSqlServer(Configuration.GetConnectionString(name: "DefaultConnection")));
+
+			services.AddDefaultIdentity<IdentityUser>()
+				.AddRoles<IdentityRole>()
+				.AddEntityFrameworkStores<ApplicationDbContext>()
+				.AddDefaultTokenProviders();
 
 			services.AddControllers();
 			services.AddSwaggerGen(c =>
@@ -34,7 +43,6 @@ namespace MJ.Solutions.Identidade.API
 			});
 		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
 			if (env.IsDevelopment())
@@ -48,6 +56,7 @@ namespace MJ.Solutions.Identidade.API
 
 			app.UseRouting();
 
+			app.UseAuthentication();
 			app.UseAuthorization();
 
 			app.UseEndpoints(endpoints =>
