0218c41298579275e29101aed23eb4ccabfdd0c5
Marcelo Jesualdo <fjesualdo@outlook.com>
2022-04-06 13:38:01 -0300

    M04V03 - Configurando a autenticação

---
+++ src/Web/Latelier.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Latelier.WebApp.MVC.Controllers
+{
+	public class IdentidadeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
