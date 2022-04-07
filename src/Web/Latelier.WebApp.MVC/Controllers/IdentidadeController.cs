628979f5e30014bba743a2762aa80d3fcfbb7541
Marcelo Jesualdo <fjesualdo@outlook.com>
2022-04-07 18:11:22 -0300

    M04V07 - Criando um cookie com base no JWT

--- src/Web/Latelier.WebApp.MVC/Controllers/IdentidadeController.cs
+++ src/Web/Latelier.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -1,6 +1,12 @@
 ﻿using Latelier.WebApp.MVC.Models;
 using Latelier.WebApp.MVC.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Latelier.WebApp.MVC.Controllers
@@ -28,10 +34,10 @@ namespace Latelier.WebApp.MVC.Controllers
 			if (!ModelState.IsValid) return View(usuarioRegistro);
 
 			// API - Login
-
 			var resposta = await _autenticacaoService.Registro(usuarioRegistro);
 
 			// Realizar login na APP
+			await RealizarLogin(resposta);
 
 			return RedirectToAction("Index", "Home");
 		}
@@ -51,10 +57,10 @@ namespace Latelier.WebApp.MVC.Controllers
 			if (!ModelState.IsValid) return View(usuarioLogin);
 
 			// API - Login
-
 			var resposta = await _autenticacaoService.Login(usuarioLogin);
 
 			// Realizar login na APP
+			await RealizarLogin(resposta);
 
 			return RedirectToAction("Index", "Home");
 		}
@@ -64,6 +70,33 @@ namespace Latelier.WebApp.MVC.Controllers
 		public async Task<IActionResult> Logout()
 		{
 			return RedirectToAction("Index", "Home");
+		}
+
+		private async Task RealizarLogin(UsuarioRespostaLogin resposta)
+		{
+			var token = ObterTokenFormatado(resposta.AccessToken);
+
+			var claims = new List<Claim>();
+			claims.Add(new Claim("JWT", resposta.AccessToken));
+			claims.AddRange(token.Claims);
+
+			var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+			var authProperties = new AuthenticationProperties
+			{
+				ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(60),
+				IsPersistent = true
+			};
+
+			await HttpContext.SignInAsync(
+					CookieAuthenticationDefaults.AuthenticationScheme,
+					new ClaimsPrincipal(claimsIdentity),
+					authProperties);
+		}
+
+		private static JwtSecurityToken ObterTokenFormatado(string jwtToken)
+		{
+			return new JwtSecurityTokenHandler().ReadToken(jwtToken) as JwtSecurityToken;
 		}
 	}
 }
