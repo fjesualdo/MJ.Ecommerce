77d5b118a8aa10691386df45c33d483b7306cd52
Marcelo Jesualdo <fjesualdo@outlook.com>
2022-04-07 10:51:57 -0300

    M04V06 - Padronizando os objetos de retorno

--- src/Web/Latelier.WebApp.MVC/Services/AutenticacaoService.cs
+++ src/Web/Latelier.WebApp.MVC/Services/AutenticacaoService.cs
@@ -15,7 +15,7 @@ namespace Latelier.WebApp.MVC.Services
 			_httpClient = httpClient;
 		}
 
-		public async Task<string> Login(UsuarioLogin usuarioLogin)
+		public async Task<UsuarioRespostaLogin> Login(UsuarioLogin usuarioLogin)
 		{
 			var loginContent = new StringContent(
 				content: JsonSerializer.Serialize(usuarioLogin),
@@ -24,12 +24,15 @@ namespace Latelier.WebApp.MVC.Services
 
 			var response = await _httpClient.PostAsync(requestUri: "https://localhost:44384/api/identidade/autenticar/", content: loginContent);
 
-			var teste = await response.Content.ReadAsStringAsync();
+			var options = new JsonSerializerOptions
+			{
+				PropertyNameCaseInsensitive = true
+			};
 
-			return JsonSerializer.Deserialize<string>(await response.Content.ReadAsStringAsync());
+			return JsonSerializer.Deserialize<UsuarioRespostaLogin>(await response.Content.ReadAsStringAsync(), options);
 		}
 
-		public async Task<string> Registro(UsuarioRegistro usuarioRegistro)
+		public async Task<UsuarioRespostaLogin> Registro(UsuarioRegistro usuarioRegistro)
 		{
 			var registroContent = new StringContent(
 				content: JsonSerializer.Serialize(usuarioRegistro),
@@ -38,7 +41,7 @@ namespace Latelier.WebApp.MVC.Services
 
 			var response = await _httpClient.PostAsync(requestUri: "https://localhost:44384/api/identidade/nova-conta/", content: registroContent);
 
-			return JsonSerializer.Deserialize<string>(await response.Content.ReadAsStringAsync());
+			return JsonSerializer.Deserialize<UsuarioRespostaLogin>(await response.Content.ReadAsStringAsync());
 		}
 
 	}
