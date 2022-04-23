d94e16e6fcbf298d89742ff9e5903c7a0779a78c
Marcelo Jesualdo <fjesualdo@outlook.com>
2022-04-23 15:29:34 -0300

    M06V04 - Objetos de valor e validações

---
+++ src/Building Blocks/MJ.Solutions.Core/Utils/StringUtils.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace MJ.Solutions.Core.Utils
+{
+	public static class StringUtils
+	{
+
+		//TODO:: MJ gerar extensions
+
+		public static string ApenasNumeros(this string str, string input)
+		{
+			return new string(input.Where(char.IsDigit).ToArray());
+		}
+	}
+}
