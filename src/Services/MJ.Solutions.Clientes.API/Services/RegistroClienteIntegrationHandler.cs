21236b0e2a22e09be3be3f15bd478b19e119b180
Marcelo Jesualdo <fjesualdo@outlook.com>
2022-04-26 00:16:20 -0300

    M07V10 - Customizacao do EventBus - Pt III

--- src/Services/MJ.Solutions.Clientes.API/Services/RegistroClienteIntegrationHandler.cs
+++ src/Services/MJ.Solutions.Clientes.API/Services/RegistroClienteIntegrationHandler.cs
@@ -1,10 +1,10 @@
-﻿using EasyNetQ;
-using FluentValidation.Results;
+﻿using FluentValidation.Results;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MJ.Solutions.Clientes.API.Application.Commands;
 using MJ.Solutions.Core.Mediator;
 using MJ.Solutions.Core.Messages.Integration;
+using MJ.Solutions.MessageBus;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,28 +13,25 @@ namespace MJ.Solutions.Clientes.API.Services
 {
 	public class RegistroClienteIntegrationHandler : BackgroundService
 	{
-		private IBus _bus;
+		private readonly IMessageBus _bus;
 		private readonly IServiceProvider _serviceProvider;
 
 
-		public RegistroClienteIntegrationHandler(IServiceProvider serviceProvider)
+		public RegistroClienteIntegrationHandler(IServiceProvider serviceProvider, IMessageBus bus)
 		{
 			_serviceProvider = serviceProvider;
+			_bus = bus;
 		}
 
 		protected override Task ExecuteAsync(CancellationToken stoppingToken)
 		{
-			_bus = RabbitHutch.CreateBus(connectionString: "host=localhost:5672");
-
-			_bus.Rpc.RespondAsync<UsuarioRegistradoIntegrationEvent, ResponseMessage>(responder: async request =>
-				new ResponseMessage(await RegistrarCliente(request))
-			);
+			_bus.RespondAsync<UsuarioRegistradoIntegrationEvent, ResponseMessage>(responder: async request => await RegistrarCliente(request));
 
 			return Task.CompletedTask;
 
 		}
 
-		private async Task<ValidationResult> RegistrarCliente(UsuarioRegistradoIntegrationEvent message)
+		private async Task<ResponseMessage> RegistrarCliente(UsuarioRegistradoIntegrationEvent message)
 		{
 			var clienteCommand = new RegistrarClienteCommand(message.Id, message.Nome, message.Email, message.Cpf);
 			ValidationResult sucesso;
@@ -45,7 +42,7 @@ namespace MJ.Solutions.Clientes.API.Services
 				sucesso = await mediator.EnviarComando(clienteCommand);
 			}
 
-			return sucesso;
+			return new ResponseMessage(sucesso);
 		}
 	}
 }
--- src/Services/MJ.Solutions.Identidade.API/Controllers/IdentidadeController.cs
+++ src/Services/MJ.Solutions.Identidade.API/Controllers/IdentidadeController.cs
@@ -1,10 +1,10 @@
-﻿using EasyNetQ;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using MJ.Solutions.Core.Messages.Integration;
 using MJ.Solutions.Identidade.API.Models;
+using MJ.Solutions.MessageBus;
 using MJ.Solutions.WebAPI.Core.Controllers;
 using MJ.Solutions.WebAPI.Core.Identidade;
 using System;
@@ -23,14 +23,14 @@ namespace MJ.Solutions.Identidade.API.Controllers
 		private readonly SignInManager<IdentityUser> _signInManager;
 		private readonly UserManager<IdentityUser> _userManager;
 		private readonly AppSettings _appSettings;
+		private readonly IMessageBus _bus;
 
-		private IBus _bus;
-
-		public IdentidadeController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager, IOptions<AppSettings> appSettings)
+		public IdentidadeController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager, IOptions<AppSettings> appSettings, IMessageBus bus)
 		{
 			_signInManager = signInManager;
 			_userManager = userManager;
 			_appSettings = appSettings.Value;
+			_bus = bus;
 		}
 
 		[HttpPost("nova-conta")]
@@ -49,7 +49,13 @@ namespace MJ.Solutions.Identidade.API.Controllers
 
 			if (result.Succeeded)
 			{
-				var sucesso = await RegistrarCliente(usuarioRegistro);
+				var clieteResult = await RegistrarCliente(usuarioRegistro);
+
+				if (!clieteResult.ValidationResult.IsValid)
+				{
+					await _userManager.DeleteAsync(user);
+					return CustomResponse(clieteResult.ValidationResult);
+				}
 
 				return CustomResponse(await GerarJwt(usuarioRegistro.Email));
 			}
@@ -156,11 +162,15 @@ namespace MJ.Solutions.Identidade.API.Controllers
 
 			var usuarioRegistrado = new UsuarioRegistradoIntegrationEvent(Guid.Parse(usuario.Id), usuarioRegistro.Nome, usuarioRegistro.Email, usuarioRegistro.Cpf);
 
-			_bus = RabbitHutch.CreateBus(connectionString: "host=localhost:5672");
-
-			var sucesso = await _bus.Rpc.RequestAsync<UsuarioRegistradoIntegrationEvent, ResponseMessage>(usuarioRegistrado);
-
-			return sucesso;
+			try
+			{
+				return await _bus.RequestAsync<UsuarioRegistradoIntegrationEvent, ResponseMessage>(usuarioRegistrado);
+			}
+			catch
+			{
+				await _userManager.DeleteAsync(usuario);
+				throw;
+			}
 		}
 	}
 }
