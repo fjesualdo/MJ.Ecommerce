ebd645d4ff72abab702c4031767c939798e45de5
Marcelo Jesualdo <fjesualdo@outlook.com>
2022-05-05 12:02:20 -0300

    M08V10 - Implementando o carrinho no front-end - pt III

---
+++ src/Web/Latelier.WebApp.MVC/Extensions/CarrinhoViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Latelier.WebApp.MVC.Models;
+using Latelier.WebApp.MVC.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Latelier.WebApp.MVC.Extensions
+{
+	public class CarrinhoViewComponent : ViewComponent
+	{
+		private readonly ICarrinhoService _carrinhoService;
+
+		public CarrinhoViewComponent(ICarrinhoService carrinhoService)
+		{
+			_carrinhoService = carrinhoService;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			return View(await _carrinhoService.ObterCarrinho() ?? new CarrinhoViewModel());
+		}
+	}
+}
