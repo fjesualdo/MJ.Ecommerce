7868c552a1a199e4419a8cc10e17b58d5eaa6d3a
Marcelo Jesualdo <fjesualdo@outlook.com>
2022-05-06 15:41:40 -0300

    M09V10 - Aplicando o voucher na API de Carrinho

--- src/Services/MJ.Solutions.Carrinho.API/Controllers/CarrinhoController.cs
+++ src/Services/MJ.Solutions.Carrinho.API/Controllers/CarrinhoController.cs
@@ -88,6 +88,20 @@ namespace MJ.Solutions.Carrinho.API.Controllers
 			return CustomResponse();
 		}
 
+		[HttpPost]
+		[Route("carrinho/aplicar-voucher")]
+		public async Task<IActionResult> AplicarVoucher(Voucher voucher)
+		{
+			var carrinho = await ObterCarrinhoCliente();
+
+			carrinho.AplicarVoucher(voucher);
+
+			_context.CarrinhoCliente.Update(carrinho);
+
+			await PersistirDados();
+			return CustomResponse();
+		}
+
 
 
 		private async Task<CarrinhoCliente> ObterCarrinhoCliente()
--- src/Services/MJ.Solutions.Carrinho.API/Migrations/20220506181647_Voucher.Designer.cs
+++ src/Services/MJ.Solutions.Carrinho.API/Migrations/20220506181647_Voucher.Designer.cs
@@ -4,14 +4,16 @@ using MJ.Solutions.Carrinho.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MJ.Solutions.Carrinho.API.Migrations
 {
     [DbContext(typeof(CarrinhoContext))]
-    partial class CarrinhoContextModelSnapshot : ModelSnapshot
+    [Migration("20220506181647_Voucher")]
+    partial class Voucher
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
