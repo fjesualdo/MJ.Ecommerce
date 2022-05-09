9a117d35393477583baa5743fcb75d7f29bf5a4a
Marcelo Jesualdo <fjesualdo@outlook.com>
2022-05-09 08:34:03 -0300

    M09V14 - Camada de application de pedido

---
+++ src/Services/MJ.Solutions.Pedidos.API/Application/Commands/PedidoCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation.Results;
+using MediatR;
+using MJ.Solutions.Core.Messaging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MJ.Solutions.Pedidos.API.Application.Commands
+{
+	public class PedidoCommandHandler : CommandHandler, IRequestHandler<AdicionarPedidoCommand, ValidationResult>
+	{
+		public Task<ValidationResult> Handle(AdicionarPedidoCommand message, CancellationToken cancellationToken)
+		{
+			// Validarção do comando
+
+			// Mapear pedido
+
+			// Aplicar voucher se houver
+
+			// Validar pedido
+
+			// Processar pagamento
+
+			// Se pagamento tudo OK!
+
+			// Adicionar Evento
+
+			// Adicionar Pedido Repositorio
+
+			// Persistir dados de pedido e voucher
+
+
+			return null;
+		}
+	}
+}
